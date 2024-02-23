29c50f4055de2448a4040559c71ad5a1d109198f
lufa23 <usuario@mind-your-business.whatever>
2024-02-23 17:04:12 +0100

    add entry point parameters

--- area_52/src/contract.rs
+++ area_52/src/contract.rs
@@ -1,9 +1,15 @@
-use cosmwasm_std::entry_point;
-use cosmwasm_std::Response;
+use cosmwasm_std::{
+    entry_point, DepsMut, Env, MessageInfo, Response
+};
 use crate::error::ContractError;
+use crate::msg::InstantiateMsg;
 
 #[entry_point]
-pub fn instantiate() -> Result<Response, ContractError>/* signature return here */ {
+pub fn instantiate(
+deps: DepsMut,
+_env: Env,
+info: MessageInfo,
+msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
     Ok(Response::default())
-
 }
