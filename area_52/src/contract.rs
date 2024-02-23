b968535d3462beac8a4d94f15fe9fd5d5815b495
lufa23 <usuario@mind-your-business.whatever>
2024-02-23 19:31:46 +0100

    saving state to blockchain

--- area_52/src/contract.rs
+++ area_52/src/contract.rs
@@ -2,14 +2,22 @@ use cosmwasm_std::{
     entry_point, DepsMut, Env, MessageInfo, Response
 };
 use crate::error::ContractError;
-use crate::msg::InstantiateMsg;
+use crate::msg::{InstantiateMsg};
+use crate::state::{config, State};
 
 #[entry_point]
 pub fn instantiate(
-deps: DepsMut,
-_env: Env,
-info: MessageInfo,
-msg: InstantiateMsg,
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
+    let state = State {
+        owner: info.sender,
+
+    };
+    
+    config(deps.storage).save(&state)?;
+    
     Ok(Response::default())
 }
