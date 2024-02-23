a5e51424902a60619de0f9d241f8ed7642d1a441
lufa23 <usuario@mind-your-business.whatever>
2024-02-23 17:16:18 +0100

    storing state

--- area_52/src/state.rs
+++ area_52/src/state.rs
@@ -1,9 +1,19 @@
 use serde::{Deserialize, Serialize};
+use cosmwasm_std::{Addr, Storage};
+use cosmwasm_storage::{ReadonlySingleton, Singleton};
 use universe::species::{SapienceScale, Sapient};
-use cosmwasm_std::{Addr};
+
+pub fn config(storage: &mut dyn Storage) -> Singleton<State> {
+
+}
+
+pub fn config_read(storage: &dyn Storage) -> ReadonlySingleton<State> {
+
+}
+
+// add the config_read signature here
 
 #[derive(Serialize, Deserialize)]
-
 pub struct State {
     pub owner: Addr,
     pub planet_name: String,
