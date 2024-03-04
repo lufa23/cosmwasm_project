252334b58376790a91e0733faa0013a444c8bad7
lufa23 <usuario@mind-your-business.whatever>
2024-03-04 18:25:18 +0100

    add ExecuteMsg contract call

--- area_52/src/contract.rs
+++ area_52/src/contract.rs
@@ -4,18 +4,13 @@ use cosmwasm_std::{
 use crate::error::ContractError;
 use crate::msg::{InstantiateMsg};
 use crate::state::{config, State};
-
-
 #[entry_point]
 pub fn query(
     deps: Deps,
     _env: Env,
-    msg: QueryMsg,
+    msg: QueryMsg
 ) -> StdResult<Binary> {
-    
 }
-
-
 
 #[entry_point]
 pub fn execute(
@@ -24,7 +19,11 @@ pub fn execute(
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-
+    match msg {
+        ExecuteMsg::SetPlanetName { to } => set_planet_name(to, deps, info),
+        ExecuteMsg::SetSapientNames { to } => set_sapient_names(to, deps, info),
+        ExecuteMsg::SetMinimumSapience { to } => set_minimum_sapience(to, deps, info),
+    }
 }
 
 #[entry_point]
@@ -39,10 +38,8 @@ pub fn instantiate(
         planet_name: msg.planet_name,
         planet_sapients: msg.planet_sapients,
         minimum_sapience: msg.minimum_sapience,
-        // add the new msg items
     };
     config(deps.storage).save(&state)?;
-    
     Ok(Response::new()
         .add_attribute("owner", state.owner)
         .add_attribute("minimum_sapience", state.minimum_sapience.as_str()))
