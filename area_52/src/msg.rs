d204dc36d2ed086cfd96184302910d1a2cf72a9e
lufa23 <usuario@mind-your-business.whatever>
2024-03-04 17:58:56 +0100

    add ExecuteMsg

--- area_52/src/msg.rs
+++ area_52/src/msg.rs
@@ -2,9 +2,15 @@ use serde::{Deserialize, Serialize};
 use universe::species::{SapienceScale, Sapient};
 
 #[derive(Serialize, Deserialize)]
+pub enum ExecuteMsg { 
+    SetPlanetName { to: String },
+    SetSapientNames { to: Vec<Sapient> },
+    SetMinimumSapience { to: SapienceScale },
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct InstantiateMsg {
     pub planet_name: String,
     pub planet_sapients: Vec<Sapient>,
     pub minimum_sapience: SapienceScale,
-
 }
