93fab42b7ed7546b8953cf8e3a5dacd631df3d77
lufa23 <usuario@mind-your-business.whatever>
2024-03-04 18:39:32 +0100

    add query_fns.rs

---
+++ area_52/src/query_fns.rs
@@ -0,0 +1,7 @@
+use cosmwasm_std::{to_binary, Binary, Deps, StdResult};
+use crate::state::config_read;
+use universe::species::{Traveler};
+
+pub fn jumpring_check(traveler: Traveler) -> StdResult<Binary> {
+
+}
