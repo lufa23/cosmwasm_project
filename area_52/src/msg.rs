add3fdc5b8229cf36e4b344446100b87d5d9ed8e
lufa23 <usuario@mind-your-business.whatever>
2024-03-04 18:46:56 +0100

    add query_msg

--- area_52/src/msg.rs
+++ area_52/src/msg.rs
@@ -1,14 +1,21 @@
 use serde::{Deserialize, Serialize};
-use universe::species::{SapienceScale, Sapient};
+use universe::species::{SapienceScale, Sapient, Traveler};
+use schemars::JsonSchema;
 
-#[derive(Serialize, Deserialize)]
-pub enum ExecuteMsg { 
+#[derive(Serialize, Deserialize, JsonSchema)]
+pub enum QueryMsg {
+    JumpRingPreCheck { traveler: Traveler },
+    MinimumSapience {},
+}
+
+#[derive(Serialize, Deserialize, JsonSchema)]
+pub enum ExecuteMsg {
     SetPlanetName { to: String },
     SetSapientNames { to: Vec<Sapient> },
     SetMinimumSapience { to: SapienceScale },
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, JsonSchema)]
 pub struct InstantiateMsg {
     pub planet_name: String,
     pub planet_sapients: Vec<Sapient>,
