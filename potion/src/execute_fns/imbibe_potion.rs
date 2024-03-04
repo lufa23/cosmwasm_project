c413273b48aeaedfad9d79f03048d42f7b869b23
lufa23 <usuario@mind-your-business.whatever>
2024-03-04 19:10:33 +0100

    implement wasm message

--- potion/src/execute_fns/imbibe_potion.rs
+++ potion/src/execute_fns/imbibe_potion.rs
@@ -1,9 +1,9 @@
 use crate::error::ContractError;
-use crate::state::{config};
-use cosmwasm_std::{
-    CosmosMsg, DepsMut, MessageInfo, Response, SubMsg,
-};
+use crate::state::{config, imbiber, Imbiber};
+use cosmwasm_std::{to_binary, CosmosMsg, DepsMut, MessageInfo, Response, SubMsg, WasmMsg};
+use universe::section31::ExecuteMsg as Section31Execute;
 use universe::species::Species;
+static SECTION31_CONTRACT_ADDR: &str = "wasm_secret_address_do_not_reveal_to_anyone";
 pub fn imbibe_potion(
     name: String,
     species: Species,
@@ -26,7 +26,15 @@ pub fn imbibe_potion(
     };    
     let key = info.sender.to_string();
     imbiber(deps.storage).save(key.as_bytes(), &cyborg)?;
-    let msg = CosmosMsg::Wasm();
+    let msg = CosmosMsg::Wasm(WasmMsg::Execute {
+        contract_addr: SECTION31_CONTRACT_ADDR.to_string(),
+        msg: to_binary(&Section31Execute::Snitch {
+            address: info.sender,
+            name: name,
+            species: species,
+        })?,
+        funds: vec![],
+    });
     let submsg = SubMsg::reply_on_error(msg, 1);
     Ok(Response::new().add_submessage(submsg))
 }
