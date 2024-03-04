bfe2eb5d02294a27bcb4d61c0ba8ad9d048a8377
lufa23 <usuario@mind-your-business.whatever>
2024-03-04 18:43:39 +0100

    implement jumpringcheck

--- area_52/src/query_fns.rs
+++ area_52/src/query_fns.rs
@@ -1,7 +1,10 @@
 use cosmwasm_std::{to_binary, Binary, Deps, StdResult};
 use crate::state::config_read;
+use crate::msg::JumpRingCheckResponse;
 use universe::species::{Traveler};
 
 pub fn jumpring_check(traveler: Traveler) -> StdResult<Binary> {
-
+    let out = to_binary(&JumpRingCheckResponse {
+        valid: traveler.cyberdized,
+    })?;
 }
