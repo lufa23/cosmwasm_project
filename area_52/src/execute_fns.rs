6f95788a958bff9ac12a78e7e4c06b6e4fc5ffba
lufa23 <usuario@mind-your-business.whatever>
2024-03-04 18:33:16 +0100

    validate msg sender

--- area_52/src/execute_fns.rs
+++ area_52/src/execute_fns.rs
@@ -1,9 +1,15 @@
 use cosmwasm_std::{DepsMut, MessageInfo, Response};
 use crate::error::ContractError;
+use crate::state::{config};
 
 pub fn set_planet_name(
     to: String,
     deps: DepsMut,
     info: MessageInfo,
 ) -> Result<Response, ContractError> {
+    let mut state = config(deps.storage).load()?;
+    if info.sender != state.owner {
+        return Err(ContractError::Unauthorized {});
+    }
+
 }
